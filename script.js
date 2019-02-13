41eb20784252e6e23f8a1d1ef854c54aa12f5b3b
DenisKharkov <denis-kharkov@inbox.ru>
2019-02-13 21:12:41 +0200

    добавил разных фото.... )))

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let slidesItem = document.querySelectorAll('.slide-item');
 let indContainer = document.querySelector('.indicators');
+let indItems = document.querySelectorAll('.indicators__item');
 let btnPausePlay = document.querySelector('#pause-play');
 let btnPrev = document.querySelector('#prev');
 let btnNext = document.querySelector('#next');
 let currentSlide = 0;
 let playStatus = true;
 let timerId = null;
-let timerInterval = 1000;
+let timerInterval = 2000;
 
 const FA_PAUSE = '<i class="fas fa-pause"></i>';
 const FA_PLAY = '<i class="fas fa-play"></i>';
@@ -19,47 +20,49 @@ document.querySelector('.controls').style.display = 'block';
 
 let goToSlide = (n) => {
 	slidesItem[currentSlide].classList.toggle('active');
+	indItems[currentSlide].classList.toggle('active');
 	currentSlide = (n + slidesItem.length) % slidesItem.length;
 	slidesItem[currentSlide].classList.toggle('active');
-}
+	indItems[currentSlide].classList.toggle('active');
+};
 
 let goToNextSlide = () => {
 	goToSlide(currentSlide + 1);
-}
+};
 
 let goToPrevSlide = () => {
 	goToSlide(currentSlide - 1);
-}
+};
 
 let startSlider = () => {
 	timerId = setInterval(goToNextSlide, timerInterval);
-}
+};
 
 let pauseSlideShow = () => {
 	btnPausePlay.innerHTML = FA_PLAY;
 	playStatus = !playStatus;
 	clearInterval(timerId);
-}
+};
 
 let playSlideShow = () => {
 	btnPausePlay.innerHTML = FA_PAUSE;
 	playStatus = !playStatus;
 	startSlider();
-}
+};
 
 let pausePlaySlideShow = () => {
 	playStatus ? pauseSlideShow() : playSlideShow();
-}
+};
 
 let clickPrevBtn = () => {
 	pauseSlideShow();
 	goToPrevSlide();
-}
+};
 
 let clickNextBtn = () => {
 	pauseSlideShow();
 	goToNextSlide();
-}
+};
 
 btnPausePlay.addEventListener('click', pausePlaySlideShow);
 btnPrev.addEventListener('click', clickPrevBtn);
@@ -69,16 +72,17 @@ let keyControlsBtn = (e) => {
 	if (e.key === SPACE) pausePlaySlideShow();
 	if (e.key === LEFT_ARROW) clickPrevBtn();
 	if (e.key === RIGTH_ARROW) clickNextBtn();
-}
+};
+
+document.addEventListener('keydown', keyControlsBtn);
+
 
 let clickIdicatorItem = (e) => {
 	let target = e.target;
-	if (target.classList.contains('indicator-item')) {
+	if (target.classList.contains('indicators__item')) {
 		pauseSlideShow();
 		goToSlide(+target.getAttribute('data-slide-to'));
 	}
-}
-
-document.addEventListener('keydown', keyControlsBtn);
+};
 
 indContainer.addEventListener('click', clickIdicatorItem);
